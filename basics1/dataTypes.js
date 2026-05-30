e337c9cf86e0cb1f237d9fd671535efd9d0c9896
Nikhil Jaiswal <nikhiljaiswal824@gmail.com>
2026-05-30 13:09:41 +0000

    datatypes

---
+++ basics1/dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // Treat all JS code as newer version
+
+/*
+alert(3 + 3)
+we are using nodejs not browser so this operation gives error but runs freely in browser console.
+*/
+
+console.log(3 + 3)
+console.log("Nikhil")   // Code Readability is the prior thing while while writing code.
+
+let name = "nikhil"
+let age = 18
+let isLoggedIn = false 
+let state;
+let city = null
+
+// Primitive Datatypes: ==========
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone Value like "city"
+// undefined => value not defined like "state"
+// symbol => to find uniqueness
+
+
+
+// Object
+
+console.log(typeof city)    // object
+console.log(typeof state)   // undefined

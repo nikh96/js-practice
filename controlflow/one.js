6fe7a5f107d4740d96032e313687b211613e42f7
Nikhil Jaiswal <nikhiljaiswal824@gmail.com>
2026-06-01 14:54:46 +0000

    one.js is created

---
+++ controlflow/one.js
@@ -0,0 +1,51 @@
+// If Statement 
+
+// if (condition) {   // Condition have to be true
+
+// }
+
+// if (false) {
+//     // This code can't executed
+// }
+
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if (temperature < 50) {
+//     console.log("less than 50")
+// } else {
+//     console.log("temperature is greater than 50")
+// }
+
+
+
+// shorthand notation
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test");  // Implicit scope
+
+// there are nested loops also
+// if (balance < 500){
+//     console.log("less than")
+// } else if (balance < 750) {
+//     console.log("less than 750")
+// } else {
+//     console.log("less than 1200")
+// }
+
+
+// const userLoggedIn = true
+// const debitCard = true 
+// const loggedInFromGoogle = false
+// const LoggedInFromEmail = true
+
+// if (userLoggedIn && debitCard) {       // && for "and" statement
+//     console.log("Allow to buy courses")
+// }
+
+// if (loggedInFromGoogle || LoggedInFromEmail) {      // || for "or" statement
+//     console.log("User logged in")
+// }
+
+

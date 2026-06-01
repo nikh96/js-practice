c6549a4ef2aebd1fa7063c87882d9d0d49a13ef4
Nikhil Jaiswal <nikhiljaiswal824@gmail.com>
2026-06-01 15:41:27 +0000

    half portion of for loop

---
+++ iterations/one.js
@@ -0,0 +1,9 @@
+// for loop basic syntax:
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i
+//     if (element == 5) {
+//         console.log("5 is best number")
+//     }
+//     console.log(element)
+// }

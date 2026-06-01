eb0287f36c70f005886e441e42d27c2c0f28e622
Nikhil Jaiswal <nikhiljaiswal824@gmail.com>
2026-06-01 15:20:56 +0000

    controlflow things in different scenarios

---
+++ controlflow/switch.js
@@ -0,0 +1,53 @@
+// Basic syntax of switch 
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("January")
+//         break;
+//     case 2:
+//         console.log("february")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+//     case 4:
+//         console.log("april")
+//         break;
+
+//     default:
+//         console.log("default case match")
+//         break;
+// }
+
+
+const month = "mar"
+
+switch (month) {
+    case "jan":
+        console.log("January")
+        break;
+    case "feb":
+        console.log("february")
+        break;
+    case "mar":
+        console.log("March")
+        break;
+    case "apr":
+        console.log("April")
+        break;
+
+    default:
+        console.log("default value is matched")
+        break;
+}

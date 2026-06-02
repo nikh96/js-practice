cbfded1aa546b31be6a68933ac75823380e5d07b
Nikhil Jaiswal <nikhiljaiswal824@gmail.com>
2026-06-02 02:00:08 +0000

    shopping in nine.js

--- iterations/nine.js
+++ iterations/nine.js
@@ -10,3 +10,26 @@ const myNums = [1, 2, 3]
 const myTotal = myNums.reduce( (acc, currval) => acc+currval, 0)
 
 console.log(myTotal)
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "python course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 4999
+    },
+    {
+        itemName: "Data Science course",
+        price: 12999
+    }
+]
+
+const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+console.log(priceToPay)

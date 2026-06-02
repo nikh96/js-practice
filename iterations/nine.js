1c5a6b912dd2ea9354a4c8ee977f14298d95580d
Nikhil Jaiswal <nikhiljaiswal824@gmail.com>
2026-06-02 01:56:02 +0000

    add on nine.js

--- iterations/nine.js
+++ iterations/nine.js
@@ -2,9 +2,11 @@
 
 const myNums = [1, 2, 3]
 
-const myTotal = myNums.reduce(function (acc, currval) {
-    console.log(`acc: ${acc} and curval ${currval}`)
-    return acc + currval 
-}, 0)
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and curval ${currval}`)
+//     return acc + currval 
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, currval) => acc+currval, 0)
 
 console.log(myTotal)

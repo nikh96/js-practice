e1d313a66502030611d3f846ad0aaa8144022c8e
Nikhil Jaiswal <nikhiljaiswal824@gmail.com>
2026-06-02 00:55:59 +0000

    one and two files are updated

--- iterations/one.js
+++ iterations/one.js
@@ -7,3 +7,48 @@
 //     }
 //     console.log(element)
 // }
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`)
+//     for (let j = 0; j <= 10; j++) {
+//         // console.log(`Inner loop value ${j} and inner loop ${i}`)
+//         console.log(i + '*' + j + ' = ' + i*j)
+        
+//     }
+    
+// }
+
+
+// let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length)
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index]
+//     console.log(element)
+    
+// }
+
+
+// Some Keywords 
+
+// break and continue keywords
+
+// for (let index = 0; index <= 20; index++) {
+//     if (index == 5){
+//         console.log("Detected 5")
+//         break
+//     }
+//     console.log(`Value of i is ${index}`)
+    
+// }
+
+
+for (let index = 0; index <= 20; index++) {
+    if (index == 5){
+        console.log("Detected 5")
+        continue
+    }
+    console.log(`Value of i is ${index}`)
+    
+}
